e291562993996bc92fbc87a293740d6a17bb7a89
Wkingxc <wkingxc@foxmail.com>
2024-01-07 21:36:48 +0800

    更新了修改配置文件的脚本

---
+++ Data/Xier/set_batch.py
@@ -0,0 +1,18 @@
+import json
+import argparse
+
+# 创建解析器并添加参数
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--batch_size', type=int, required=True, help='Batch size to set')
+args = parser.parse_args()
+
+# 读取json文件
+with open('config.json', 'r') as file:
+    data = json.load(file)
+
+# 修改batch_size的值
+data['train']['batch_size'] = args.batch_size
+
+# 将修改后的数据写回文件
+with open('config.json', 'w') as file:
+    json.dump(data, file, indent=2)
---
+++ Data/Xier/set_yml.py
@@ -0,0 +1,38 @@
+import argparse
+import yaml
+
+def update_config_file(bert_processes, emo_processes, train_processes):
+    # 读取config.yml文件
+    with open('config.yml', 'r') as file:
+        config_data = yaml.safe_load(file)
+
+    # 读取../config.yml文件
+    with open('../../config.yml', 'r') as file:
+        config_data2 = yaml.safe_load(file)
+
+    # 更新相应字段的值
+    config_data['bert_gen']['num_processes'] = bert_processes
+    config_data['emo_gen']['num_processes'] = emo_processes
+    config_data['train_ms']['num_processes'] = train_processes
+    config_data2['bert_gen']['num_processes'] = bert_processes
+    config_data2['emo_gen']['num_processes'] = emo_processes
+    config_data2['train_ms']['num_processes'] = train_processes
+
+    # 将更新后的数据写回文件
+    with open('config.yml', 'w') as file:
+        yaml.dump(config_data, file)
+    # 将更新后的数据写回文件
+    with open('../../config.yml', 'w') as file:
+        yaml.dump(config_data2, file)
+
+    print("config.yml文件已成功更新。")
+    print("根目录config.yml文件已成功更新。")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Update config.yml with specified processes.")
+    parser.add_argument("-b", type=int, default=2, help="Number of processes for bert_gen.")
+    parser.add_argument("-e", type=int, default=2, help="Number of processes for emo_gen.")
+    parser.add_argument("-t", type=int, default=2, help="Number of processes for train_ms.")
+    args = parser.parse_args()
+    update_config_file(args.b, args.e, args.t)
