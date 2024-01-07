d9e0f9676774b6fba930bcd134c455f9cbfa8984
Wkingxc <wkingxc@foxmail.com>
2024-01-07 22:08:01 +0800

    更改大小写规范

--- Data/Xier/set_batch.py
+++ Data/Xier/set_batch.py
@@ -10,9 +10,12 @@ args = parser.parse_args()
 with open('config.json', 'r') as file:
     data = json.load(file)
 
+data = json.loads(json.dumps(data).replace('false', 'False').replace('true', 'True'))
+
 # 修改batch_size的值
 data['train']['batch_size'] = args.batch_size
 
+data = json.loads(json.dumps(data).replace('False', 'false').replace('True', 'true'))
 # 将修改后的数据写回文件
 with open('config.json', 'w') as file:
     json.dump(data, file, indent=2)
--- Data/Xier/set_yml.py
+++ Data/Xier/set_yml.py
@@ -5,10 +5,13 @@ def update_config_file(bert_processes, emo_processes, train_processes):
     # 读取config.yml文件
     with open('config.yml', 'r') as file:
         config_data = yaml.safe_load(file)
-
+    
     # 读取../config.yml文件
     with open('../../config.yml', 'r') as file:
         config_data2 = yaml.safe_load(file)
+
+    config_data = json.loads(json.dumps(data).replace('false', 'False').replace('true', 'True'))
+    config_data2 = json.loads(json.dumps(data).replace('false', 'False').replace('true', 'True'))
 
     # 更新相应字段的值
     config_data['bert_gen']['num_processes'] = bert_processes
@@ -18,6 +21,8 @@ def update_config_file(bert_processes, emo_processes, train_processes):
     config_data2['emo_gen']['num_processes'] = emo_processes
     config_data2['train_ms']['num_processes'] = train_processes
 
+    config_data = json.loads(json.dumps(data).replace('False', 'false').replace('True', 'true'))
+    config_data2 = json.loads(json.dumps(data).replace('False', 'false').replace('True', 'true'))
     # 将更新后的数据写回文件
     with open('config.yml', 'w') as file:
         yaml.dump(config_data, file)
