d9e02fa0120b0845883b9d0775962c33efa343a6
Wking-desktop <73375688+Wkingxc@users.noreply.github.com>
2024-01-08 23:37:17 +0800

    可以配置bs和epoch

--- Data/EX1/set_json.py
+++ Data/EX1/set_json.py
@@ -3,7 +3,8 @@ import argparse
 
 # 创建解析器并添加参数
 parser = argparse.ArgumentParser()
-parser.add_argument('-b', '--batch_size', type=int, required=True, help='Batch size to set')
+parser.add_argument('-b', '--batch_size', type=int, default=4, help='Batch size to set')
+parser.add_argument('-e', '--epochs', type=int, default=100 , help='Epochs to set')
 args = parser.parse_args()
 
 # 读取json文件为字符串
@@ -12,6 +13,7 @@ with open('config.json', 'r') as file:
 
 # 修改batch_size的值
 data['train']['batch_size'] = args.batch_size
+data['train']['epochs'] = args.epochs
 
 # 将修改后的数据写回文件
 with open('config.json', 'w') as file:
